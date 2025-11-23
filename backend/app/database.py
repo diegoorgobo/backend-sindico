b87e14b3dc205d69b38154253b32c4e5c393fd35
Diego Gobo <diegoorgobo@gmail.com>
2025-11-23 18:48:02 -0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -5,7 +5,7 @@ import os
 
 # Em produção, use variáveis de ambiente. Ex: os.getenv("DATABASE_URL") 
 # String de conexão de exemplo (substitua pela sua do Supabase) ------------ senha: JRhKVTLayVK1cySk
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:JRhKVTLayVK1cySk@db.jqlygddtjkvtuckwpucp.supabase.co:5432/postgres"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres.jqlygddtjkvtuckwpucp:JRhKVTLayVK1cySk@aws-1-sa-east-1.pooler.supabase.com:6543/postgres"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -17,4 +17,5 @@ def get_db():
     try:
         yield db
     finally:
+
         db.close()
